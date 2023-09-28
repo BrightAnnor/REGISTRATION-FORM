18724d1489f23d815f7b0b9eb45c5db72009c5ef
BrightAnnor <annor170bright@gmail.com>
2023-09-28 14:59:35 +0000

    update:setting theconnection to the database

---
+++ database/dbConnect.js
@@ -0,0 +1,13 @@
+const {Sequelize} = require('sequelize');
+
+//DATABASE Credentials
+const DB_NAME = process.env.DATABASE_NAME
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+
+const sequelize = new Sequelize(DB_NAME,DB_USER,DB_PASS,{
+    host:'localhost',
+    dialect:'mysql'
+});
+
+module.exports = sequelize
